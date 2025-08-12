b4e44076ae3fa406024c867e718ca2e3381dafd4
Renan Garcia <renanrgarcia@gmail.com>
2025-08-12 13:55:29 -0300

    Add implementation for Longest Palindromic Substring with project files

---
+++ leetCodes/5. Longest Palindromic Substring/Program.cs
@@ -0,0 +1,26 @@
+﻿string s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+
+Console.WriteLine(LongestPalindrome(s));
+
+static string LongestPalindrome(string s)
+{
+    var greater = "";
+    var lengthS = s.Length;
+
+    if (lengthS == 1)
+        return s;
+
+    for (var i = 0; i < lengthS; i++)
+    {
+        for (var j = 1; j <= lengthS - i; j++)
+        {
+            var reversedString = new string(s.Substring(i, j).Reverse().ToArray());
+            if (s.Substring(i, j).Equals(reversedString))
+            {
+                greater = s.Substring(i, j).Length > greater.Length ? s.Substring(i, j) : greater;
+            }
+        }
+    }
+
+    return greater;
+}
